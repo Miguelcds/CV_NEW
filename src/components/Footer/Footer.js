92c4626b2014aea387181ab9c699f55c0b589b12
Joao Miguel Costa <miguel-cds@hotmail.com>
2026-01-20 16:37:18 +0100

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
    footer.innerHTML =`
 <h2> <a href="mailto:miguel-cds@hotmail.com"> Contact! </a> </h2>
 <div>
-<a href="">${Button("/twitter.png", "Twitter")}</a>
+<a href="#">${Button("/twitter.png", "Twitter")}</a>
 <a href="https://github.com/Miguelcds">${Button("/githubb.png", "@Miguelcds")}</a>
 <a href="https://linkedin.com/in/miguelcds">${Button("/linkedin.png", "Miguelcds")}</a>
 <a href="https://t.me/Micosts">${Button("/telegram.png", "Telegram")}</a>
